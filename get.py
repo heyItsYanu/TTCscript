42ce77300b572e29104041cff481da5d0c4b9040
heyItsYanu <93751294+heyItsYanu@users.noreply.github.com>
2021-11-05 03:54:43 +0100

    Create get.py
    Initial version of the script

---
+++ get.py
@@ -0,0 +1,24 @@
+import requests
+import os
+import sys
+from zipfile import ZipFile
+
+url = 'https://' + sys.argv[1] + '.tamrieltradecentre.com/download/PriceTable'
+addon_dir = 'C:\\Users\\' + os.getlogin() + '\\Documents\\Elder Scrolls Online\\live\\AddOns\\TamrielTradeCentre\\'
+table_zip = addon_dir + 'PriceTable.zip'
+
+print()
+if os.path.exists(table_zip):
+	os.remove(table_zip)
+	print("deleted")
+else:
+	print("The file does not exist")
+
+download = requests.get(url, allow_redirects=True)
+
+with open(table_zip, 'wb') as downloaded:
+	downloaded.write(download.content)
+
+zf = ZipFile(table_zip, 'r')
+zf.extractall(addon_dir)
+zf.close()
